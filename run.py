b85645fe3028ace506ca4b279b3985f805b57696
Rachel Walmsley <rachwalm75@gmail.com>
2024-09-28 19:01:37 +0100

    Complete data processing in run.py and put data into index.html

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -35,23 +35,43 @@ def process_twentyfour_submissions(name_and_scores):
     # comprehension to return user and the scores are sorted highest to lowest and cut at 24 entries
     valid_users = {name: sorted(scores, reverse=True)[:24] for name, scores in name_and_scores.items()}
     return valid_users
-    
+
+def to_dict(lsts):
+    res_dict = {}
+    for lst in lsts:
+        keys = str(lst[::2])[2:-3]
+        values = lst[1::2]
+        # print(keys)
+        # print(values)
+        res_dict[keys] = values
+    print(res_dict)
+    return res_dict
+ 
 def allowed_sum_totals(submissions):
     # comprehension to return user and sum of scores
-    sum_totals = {name: sum(submission) for name, submission in submissions.items()}
-    print(sum_totals)    
-        
-    
-# Use decorators for routing
-# @app.route("/")
-# def index():
-#     # Load scores from provided JSON file for 
-#     return render_template("index.html")
+    sums = {name: sum(submission) for name, submission in submissions.items()}
+    return sums
 
+  
+    
+def ranking(unordered):
+    # using lambda function to select the value rather than key for sorting and reverse give highest first
+    ordered = sorted(unordered.items(), key=lambda x: x[1], reverse=True)
+    return ordered
+    
 def main():
     score_list = load_scores()
     relevant_data = remove_irrelevant(score_list)
     top_allowed_submissions = process_twentyfour_submissions(relevant_data)
     sum_totals = allowed_sum_totals(top_allowed_submissions)
+    sum_rank_list = ranking(sum_totals)
+    sum_rank = to_dict(sum_rank_list)
+    print(sum_rank)
+
+    # Use decorators for routing
+    @app.route("/")
+    def index():
+        # Load scores from provided JSON file for 
+        return render_template("index.html", sum_rank=sum_rank)
     
 main()
